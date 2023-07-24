3997adac6b0858a76d5506822f1a6b34521d04df
zahara-Rahmanchy <zaharachy72@gmail.com>
2023-07-24 19:45:33 +0600

    diabetes prediction shown

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,20 +1,18 @@
-import { Outlet, useLocation } from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import Navbar from "../pages/Shared/NavBar/Navbar";
 
-
 const Main = () => {
-    const location=useLocation();
-    console.log(location);
-    const NoNavbarAndFooter=location.pathname.includes('login')||location.pathname.includes('SignUp');
-    return (
-        <div>
-           {NoNavbarAndFooter || <Navbar></Navbar>}
-            <Outlet></Outlet>
-            {NoNavbarAndFooter ||<Footer></Footer>}
-            
-        </div>
-    );
+  // const location=useLocation();
+  // console.log(location);
+  // const NoNavbarAndFooter=location.pathname.includes('login')||location.pathname.includes('SignUp');
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
